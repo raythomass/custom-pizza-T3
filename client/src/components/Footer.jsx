64275c8cfd119553bca7f2a0900c5ab7dd525d96
Colby Robins <colbyr2891@gmail.com>
2024-02-08 20:42:39 -0700

    Added footer to home page.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,19 +1,25 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-
 const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
-        )}
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-info">
+          <h4>Contact Us</h4>
+          <p>123 Pizza Street</p>
+          <p>Tucson, AZ 111111</p>
+          <p>Phone: 555-555-5555</p>
+          <p>Email: custompizzas@google.com</p>
+        </div>
+        <div className="footer-social">
+          <h4>Follow Us</h4>
+          <ul>
+            <li><a href="https://www.facebook.com/" target="-blank" rel="">Facebook</a></li>
+            <li><a href="https://www.instagram.com/" target="-blank" rel="">Instagram</a></li>
+            <li><a href="https://twitter.com/" target="-blank" rel="">Twitter</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Pizza Website. All Rights Reserved.</p>
       </div>
     </footer>
   );
